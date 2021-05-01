52a1f0915dcfd12f9ddcb939e019e788c6a8ba5a
Adish Rao <adish.rao98@gmail.com>
2021-05-01 20:02:34 +0530

    Changed formatting

--- WordFilter.py
+++ WordFilter.py
@@ -1,7 +1,8 @@
-#https://leetcode.com/problems/prefix-and-suffix-search/
+# https://leetcode.com/problems/prefix-and-suffix-search/
 
 class WordFilter:
     words_dict = {}
+
     def __init__(self, words: List[str]):
         for i in range(len(words)):
             word = words[i]
@@ -10,7 +11,7 @@ class WordFilter:
                 for k in range(lenw):
                     hash_word = word[-j:]+'#'+word[:lenw-k]
                     self.words_dict[hash_word] = i
-                    
+
     def f(self, prefix: str, suffix: str) -> int:
         find_word = suffix+'#'+prefix
         if find_word in self.words_dict:
